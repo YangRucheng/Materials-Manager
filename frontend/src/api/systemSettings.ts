8dafc66eaff747ed194606665dcf95a8ce8fd134
Sakana <admin@yangrucheng.top>
2026-07-26 13:29:11 +0800

    fix: 使用同源小程序码重定向

--- frontend/src/api/systemSettings.ts
+++ frontend/src/api/systemSettings.ts
@@ -1,11 +1,7 @@
 import { apiClient } from './client'
-import type { ImageAccelerationSettings, MiniProgramCodeSettings } from './generated'
+import type { ImageAccelerationSettings } from './generated'
 
 export const systemSettingsApi = {
-  miniProgramCode: () =>
-    apiClient
-      .get<MiniProgramCodeSettings>('/system-settings/mini-program-code')
-      .then((response) => response.data),
   imageAcceleration: () =>
     apiClient
       .get<ImageAccelerationSettings>('/system-settings/image-acceleration', { timeout: 3000 })
