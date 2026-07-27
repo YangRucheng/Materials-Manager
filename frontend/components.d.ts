10f3c07b8dca452eba3cd1a7193ff96934f5d238
Sakana <admin@yangrucheng.top>
2026-07-27 09:21:28 +0800

    优化申购计划新建表单 (#100)

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -19,6 +19,8 @@ declare module 'vue' {
     NButton: typeof import('naive-ui')['NButton']
     NCard: typeof import('naive-ui')['NCard']
     NCheckbox: typeof import('naive-ui')['NCheckbox']
+    NCollapse: typeof import('naive-ui')['NCollapse']
+    NCollapseItem: typeof import('naive-ui')['NCollapseItem']
     NConfigProvider: typeof import('naive-ui')['NConfigProvider']
     NDataTable: typeof import('naive-ui')['NDataTable']
     NDatePicker: typeof import('naive-ui')['NDatePicker']
